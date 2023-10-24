23a57dfb2608bf1429e8d66efcc9b0ed70109228
Mohak Bajaj <bmohak87@gmail.com>
2023-10-24 19:52:43 +0530

    Project Init 👨‍💻

---
+++ server/src/index.ts
@@ -0,0 +1,24 @@
+import { Elysia, t} from "elysia";
+
+const app = new Elysia();
+
+app.get("/", () => "Server is Up and Running.");
+
+app.ws('/notes', {
+        // validate incoming message
+        body: t.Object({
+            message: t.String()
+        }),
+        message(ws, { message }) {
+            ws.send({
+                message,
+                time: Date.now()
+            })
+        }
+      }
+    )
+
+app.listen(3000);
+
+console.log(`🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`)
+
