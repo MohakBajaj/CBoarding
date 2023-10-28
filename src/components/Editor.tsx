31786a81a6bd424c4e22504cec3fb23961d6b571
Mohak Bajaj <bmohak87@gmail.com>
2023-10-28 22:09:53 +0530

    Added Ai and Blob uploads

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { Editor as NovelEditor } from "novel";
+import { Editor as CBoardingEditor } from 'cboarding-note-editor';
 
 export function Editor({ id }: { id: string }) {
     const [saveStatus, setSaveStatus] = useState("Saved");
@@ -11,9 +11,8 @@ export function Editor({ id }: { id: string }) {
             <div className="absolute right-5 top-5 mb-2 rounded-lg px-2 py-1 text-sm dark:text-black bg-stone-200 dark:bg-white/75">
                 {saveStatus}
             </div>
-            <NovelEditor
+            <CBoardingEditor
                 className="w-[85vw] h-[82vh] border rounded-sm overflow-y-scroll"
-                defaultValue={"Start typing your note here..."}
                 storageKey={`note-${id}`}
                 onUpdate={() => {
                     setSaveStatus("Unsaved");
