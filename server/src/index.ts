26bbe6deafa224d98ed13df0638c8be30cbe2dd1
Mohak Bajaj <bmohak87@gmail.com>
2023-10-28 22:18:51 +0530

    server port changed to 4000

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,24 +1,24 @@
-import { Elysia, t} from "elysia";
+import { Elysia, t } from "elysia";
 
 const app = new Elysia();
 
 app.get("/", () => "Server is Up and Running.");
 
-app.ws('/notes', {
-        // validate incoming message
-        body: t.Object({
-            message: t.String()
-        }),
-        message(ws, { message }) {
-            ws.send({
-                message,
-                time: Date.now()
-            })
-        }
-      }
-    )
+app.ws("/notes", {
+  // validate incoming message
+  body: t.Object({
+    message: t.String(),
+  }),
+  message(ws, { message }) {
+    ws.send({
+      message,
+      time: Date.now(),
+    });
+  },
+});
 
-app.listen(3000);
+app.listen(4000);
 
-console.log(`🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`)
-
+console.log(
+  `🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`
+);
